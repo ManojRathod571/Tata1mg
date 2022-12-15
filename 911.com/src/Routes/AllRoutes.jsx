d65930c16ae6ea4c3774f942336fb11969294361
Rohit Kumar <rajr54827@gmail.com>
2022-12-15 10:44:23 +0530

    fixed_folder_structure

---
+++ 911.com/src/Routes/AllRoutes.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Signup from '../pages/Signup'
+
+const AllRoutes = () => {
+  return (
+    <>
+      <Routes>
+        <Route path='/signup' element={<Signup/>}/>
+      </Routes>
+    </>
+  )
+}
+
+export default AllRoutes
