5d17d0c8f172e97939c2dfd411084ca0a3be5b23
vivek gupta <bittubieber19@gmail.com>
2022-12-16 22:06:13 +0530

    Day-4 Task

---
+++ 911.com/src/components/HealthCenter/ItemCollection.js
@@ -0,0 +1,418 @@
+
+export const Data =
+    [
+        {
+            id: 1,
+            name: "Back Pain",
+            image: "https://onemg.gumlet.io/a_ignore,w_64,h_64,q_auto,f_auto/dvao23wkdmv67u0hbja6.png",
+            des: "Back pain is one of the most common pain complaints that almost everyone has had ...",
+        },
+        {
+            id: 2,
+            name: "Bloating",
+            image: "https://onemg.gumlet.io/a_ignore,w_64,h_64,q_auto,f_auto/qxnql1fvsyzgdrqn7rcg.png",
+            des: "A lot of people complain about gas trouble but don't take it seriously because th...            ",
+        },
+        {
+            id: 3,
+            name: "Blood Cancer",
+            image: "https://onemg.gumlet.io/a_ignore,w_64,h_64,q_auto,f_auto/aars3maz6zbku5w8wbxu.png",
+            des: "Blood cancer, also known as leukemia, is a condition in which there is an uncontr...   ",
+        },
+        {
+            id: 4,
+            name: "Body Ache",
+            image: "https://onemg.gumlet.io/a_ignore,w_64,h_64,q_auto,f_auto/lodvywg4pb8xxgog7eu8.png",
+            des: "A feeling of generalized, dull aching pain all over the body that may or may not ...  ",
+        },
+        {
+            id: 5,
+            name: "Breast Cancer",
+            image: "https://onemg.gumlet.io/a_ignore,w_64,h_64,q_auto,f_auto/jyxjyz6x6gg4ikby4jrr.png",
+            des: "Any changes in the breast or nipples, lumps in the armpit, pain in the breast or ...            ",
+        },
+        {
+            id: 6,
+            name: "Chickenpox",
+            image: "https://onemg.gumlet.io/a_ignore,w_64,h_64,q_auto,f_auto/rximdbbwbsitjgk9pbau.png",
+            des: "Chickenpox is a viral infection caused by the varicella zoster virus (VZV, also k...                ",
+        },
+        {
+            id: 7,
+            name: "Colon Cancer",
+            image: "https://onemg.gumlet.io/a_ignore,w_64,h_64,q_auto,f_auto/f2fp1nuk0rupazsjnfpn.png",
+            des: "Colon cancer is a condition in which there is an uncontrolled growth and multipli...            ",
+        },
+        {
+            id: 8,
+            name: "Corns",
+            image: "https://onemg.gumlet.io/a_ignore,w_64,h_64,q_auto,f_auto/marketing/3417b2d7-8d1a-4aed-b72a-8e50df58d3c8.jpg",
+            des: "Corns are calloused areas of skin that develop to protect skin from damage agains...            ",
+        },
+        {
+            id: 9,
+            name: "Cough",
+            image: "https://onemg.gumlet.io/a_ignore,w_64,h_64,q_auto,f_auto/z6ajebybzlv1fh2wwp6d.png",
+            des: "Cough is a protective reflex that expels mucus, bacteria, and other foreign subst...            ",
+        },
+        {
+            id: 10,
+            name: "Cuts and puncture wounds",
+            image: "https://onemg.gumlet.io/a_ignore,w_64,h_64,q_auto,f_auto/njqigminvri3wvpglgtu.png",
+            des: "Cuts and puncture wounds, now known as spectrum disorder, refers to a broad range of c...",
+        },
+        {
+            id: 11,
+            name: "Dandruff",
+            image: "https://onemg.gumlet.io/a_ignore,w_64,h_64,q_auto,f_auto/vcybtatmjv5zoesspoh5.png",
+            des: "Dandruff is a common hair problem that needs no introduction. It affects around 5...            ",
+        },
+        {
+            id: 19,
+            name: "Dengue",
+            image: "https://onemg.gumlet.io/a_ignore,w_64,h_64,q_auto,f_auto/bubm1qy2blhj7soyvp2o.png",
+            des: "Dengue is a mosquito-borne viral disease. It spreads from one person to the other...            ",
+        },
+        {
+            id: 20,
+            name: "Dental Pain",
+            image: "https://onemg.gumlet.io/a_ignore,w_64,h_64,q_auto,f_auto/bdvaxv1xkthsjvjdfrct.png",
+            des: "Most of us might have experienced toothache at least once in our lifetime. A toot...            ",
+        },
+        {
+            id: 21,
+            name: "Diabetes",
+            image: "https://onemg.gumlet.io/a_ignore,w_64,h_64,q_auto,f_auto/wvxxcd6w6rz3rthn4r47.png",
+            des: "Diabetes, also known as diabetes mellitus, is a long-term metabolic disorder that...            ",
+        },
+        {
+            id: 24,
+            name: "Dizziness",
+            image: "https://onemg.gumlet.io/a_ignore,w_64,h_64,q_auto,f_auto/gwtwmimh3ihtykc49yaz.png",
+            des: "Dizziness is a sensation of feeling off-balance, lightheaded, or giddy. Dizziness...            ",
+        },
+        {
+            id: 22,
+            name: "Dry Skin",
+            image: "https://onemg.gumlet.io/a_ignore,w_64,h_64,q_auto,f_auto/iimrcn4vtr8wkar7ghwp.png",
+            des: "Dry skin refers to rough, flaky, itchy, or scaly skin. It is caused by disturbed ...           ",
+        },
+        {
+            id: 23,
+            name: "Dysentery",
+            image: "https://onemg.gumlet.io/a_ignore,w_64,h_64,q_auto,f_auto/rqyjh0ow0enarwfa9zdf.png",
+            des: "Dysentery is a painful intestinal infection that causes loose and watery stools a...           ",
+        },
+
+        {
+            id: 12,
+            name: "Acidity",
+            image: "https://onemg.gumlet.io/a_ignore,w_64,h_64,q_auto,f_auto/nc1b2nmmsjm0cpzzwuri.png",
+            des: "Acidity is one of the most common ailments that almost every one of us might have...",
+        },
+        {
+            id: 13,
+            name: "Acne",
+            image: "https://onemg.gumlet.io/a_ignore,w_64,h_64,q_auto,f_auto/r1lq3ouhxzhaeg2asehv.png",
+            des: "Acne is a common condition, which most of us have dealt with at some point in our...",
+        },
+        {
+            id: 14,
+            name: "Allergic Conditions",
+            image: "https://onemg.gumlet.io/a_ignore,w_64,h_64,q_auto,f_auto/w7jst3btu1n9jevpt9wm.png",
+            des: "Allergies are caused when the body’s immune system responds abnormally to an exte...",
+        },
+        {
+            id: 17,
+            name: "Amenorrhea",
+            image: "https://onemg.gumlet.io/a_ignore,w_64,h_64,q_auto,f_auto/agflfkldbzvjh7zohjdw.png",
+            des: "Amenorrhea is defined as the absence of menstruation during the reproductive year...",
+        },
+        {
+            id: 15,
+            name: "Anxiety",
+            image: "https://onemg.gumlet.io/a_ignore,w_64,h_64,q_auto,f_auto/xd0xxmvdwxnfxk8d3lig.png",
+            des: "Anxiety disorders are the most common types of mental health conditions. Accordin...",
+        },
+        {
+            id: 16,
+            name: "Asthma",
+            image: "https://onemg.gumlet.io/a_ignore,w_64,h_64,q_auto,f_auto/djlycbpy6uwh8tfiv9sf.png",
+            des: "Asthma is a long term condition in which air passages in the lungs become narrow ...",
+        },
+        {
+            id: 18,
+            name: "Autism",
+            image: "https://onemg.gumlet.io/a_ignore,w_64,h_64,q_auto,f_auto/marketing/2242beaf-32e3-4512-b1d4-7e0f2b1e8723.jpg",
+            des: "Autism, now known as autism spectrum disorder (ASD), refers to a broad range of c...",
+        },
+        {
+            id: 25,
+            name: "Ear Pain",
+            image: "https://onemg.gumlet.io/a_ignore,w_64,h_64,q_auto,f_auto/n0ietfjzlgvjbbqtscyx.png",
+            des: "Ear pain or earache can occur due to infections and inflammation of the external,...     ",
+        },
+        {
+            id: 26,
+            name: "Eczema",
+            image: "https://onemg.gumlet.io/a_ignore,w_64,h_64,q_auto,f_auto/z0l17myuqnplqex6xmjf.png",
+            des: "Eczema is a common chronic skin condition characterized by dry, intensely itchy, ...        ",
+        },
+        {
+            id: 27,
+            name: "Epilepsy",
+            image: "https://onemg.gumlet.io/a_ignore,w_64,h_64,q_auto,f_auto/c6jxnzn5l6fx4tfckpzd.png",
+            des: "Epilepsy is a common neurological disease that affects around 50 million people w...        ",
+        },
+        {
+            id: 28,
+            name: "Fistula",
+            image: "https://onemg.gumlet.io/a_ignore,w_64,h_64,q_auto,f_auto/mzh1jo0f3m08t3bidhox.png",
+            des: "A fistula is an abnormal passage that connects two organs or cavities in the body...        ",
+        },
+        {
+            id: 29,
+            name: "Food Allergies",
+            image: "https://onemg.gumlet.io/a_ignore,w_64,h_64,q_auto,f_auto/xdrwwnx9rftta3whdcxg.png",
+            des: "If you have a food allergy, your immune system mistakenly identifies a food prote...        ",
+        },
+        {
+            id: 30,
+            name: "Gastroenteritis",
+            image: "https://onemg.gumlet.io/a_ignore,w_64,h_64,q_auto,f_auto/qcwifavkeftp5ilcqzml.png",
+            des: "Gastroenteritis is inflammation in the digestive system including the stomach, sm...        ",
+        },
+        {
+            id: 31,
+            name: "Goiter",
+            image: "https://onemg.gumlet.io/a_ignore,w_64,h_64,q_auto,f_auto/fojdhvfwjlkel7jxzygg.png",
+            des: "Goiter refers to swelling of the thyroid gland that appears like a lump in the ne...       ",
+        },
+        {
+            id: 31,
+            name: "Gout",
+            image: "https://onemg.gumlet.io/a_ignore,w_64,h_64,q_auto,f_auto/zalsjy4dgmclrx0s6ozs.png",
+            des: "Gout is one of the most common and painful forms of arthritis. It causes inflamma...        ",
+        },
+        {
+            id: 32,
+            name: "Gout",
+            image: "https://onemg.gumlet.io/a_ignore,w_64,h_64,q_auto,f_auto/zalsjy4dgmclrx0s6ozs.png",
+            des: "Gout is one of the most common and painful forms of arthritis. It causes inflamma...        ",
+        },
+        {
+            id: 33,
+            name: "Hair Loss",
+            image: "https://onemg.gumlet.io/a_ignore,w_64,h_64,q_auto,f_auto/lhpgjhokz2jbsme0lwef.png",
+            des: "Generally, people shed around 50 to 100 single hair per day. Hair shedding is a n...     ",
+        },
+        {
+            id: 34,
+            name: "Headache",
+            image: "https://onemg.gumlet.io/a_ignore,w_64,h_64,q_auto,f_auto/fcxmsixbdlhgtmqoby06.png",
+            des: "We all have headaches every now and then. Most of us usually tend to ignore them ...        ",
+        },
+        {
+            id: 35,
+            name: "Heart Attack",
+            image: "https://onemg.gumlet.io/a_ignore,w_64,h_64,q_auto,f_auto/mg5xjrl4b2vt2csgxkfe.png",
+            des: "Myocardial infarction, commonly known as heart attack, occurs when blood flow to ...        ",
+        },
+        {
+            id: 36,
+            name: "HIV Infection",
+            image: "https://onemg.gumlet.io/a_ignore,w_64,h_64,q_auto,f_auto/ajaqvrfuwv6s798dmsii.png",
+            des: "HIV (Human Immunodeficiency Virus) attacks the immune system of the body and lead...        ",
+        },
+
+        {
+            id: 37,
+            name: "Hypercalcemia",
+            image: "https://onemg.gumlet.io/a_ignore,w_64,h_64,q_auto,f_auto/vnketedtropqbmnbq2jq.png",
+            des: "Hypercalcemia is a condition in which the calcium level in the blood is above nor...        ",
+        },
+        {
+            id: 38,
+            name: "Indigestion",
+            image: "https://onemg.gumlet.io/a_ignore,w_64,h_64,q_auto,f_auto/qldjcmidhfef1vxskuh4.png",
+            des: "A sensation of pain or discomfort in the upper abdominal region accompanied by a ...        ",
+        },
+        {
+            id: 39,
+            name: "Influenza (flu)",
+            image: "https://onemg.gumlet.io/a_ignore,w_64,h_64,q_auto,f_auto/a6j7usigivy8ukdd4hdo.png",
+            des: "Influenza, most commonly referred to as the “flu” is a contagious viral infection...        ",
+        },
+        {
+            id: 40,
+            name: "Insomnia",
+            image: "https://onemg.gumlet.io/a_ignore,w_64,h_64,q_auto,f_auto/qtabtvsntgvseilkmax8.png",
+            des: "In simple terms, insomnia is characterized by dissatisfaction with sleep quantity...        ",
+        },
+        {
+            id: 41,
+            name: "Iron deficiency anemia",
+            image: "https://onemg.gumlet.io/a_ignore,w_64,h_64,q_auto,f_auto/fqot9gyjlwzwre9z6sod.png",
+            des: "Iron deficiency anemia, as the name suggests, is a type of anemia in which there ...        ",
+        },
+        {
+            id: 42,
+            name: "Japanese encephalitis",
+            image: "https://onemg.gumlet.io/a_ignore,w_64,h_64,q_auto,f_auto/eb5tzem6vtcjcckgji3z.png",
+            des: "Japanese encephalitis (JE) is a vector borne disease caused by the Japanese encep...        ",
+        },
+        {
+            id: 43,
+            name: "Jaundice",
+            image: "https://onemg.gumlet.io/a_ignore,w_64,h_64,q_auto,f_auto/tegutv6gd7vcxqm5kyki.png",
+            des: "Jaundice is a condition characterized by yellowish discoloration of the skin, scl...        ",
+        },
+        {
+            id: 44,
+            name: "Joint pain",
+            image: "https://onemg.gumlet.io/a_ignore,w_64,h_64,q_auto,f_auto/bgkolxhtj4ol5blfxvfk.png",
+            des: "Joint pain is a perception of pain or discomfort in the joints. While pain in the...        ",
+        },
+        {
+            id: 45,
+            name: "Keratitis",
+            image: "https://onemg.gumlet.io/a_ignore,w_64,h_64,q_auto,f_auto/ktqknm5dlgek6qckqxyr.png",
+            des: "Keratitis is the inflammation of the cornea which is the clear outer layer of the...        ",
+        },
+        {
+            id: 46,
+            name: "Kidney cancer",
+            image: "https://onemg.gumlet.io/a_ignore,w_64,h_64,q_auto,f_auto/tlv73e9qgq4qklmpnrkf.png",
+            des: "Kidney cancer occurs when healthy cells in one or both kidneys grow out of contro...        ",
+        },
+        {
+            id: 47,
+            name: "Kidney failure",
+            image: "https://onemg.gumlet.io/a_ignore,w_64,h_64,q_auto,f_auto/jstxknkwa3tsopyrvc8h.png",
+            des: "Kidney failure is a medical condition in which one or both the kidneys stop funct...        ",
+        },
+        {
+            id: 48,
+            name: "kidney Stone",
+            image: "https://onemg.gumlet.io/a_ignore,w_64,h_64,q_auto,f_auto/marketing/1059b41d-0bf3-4d43-9837-fd2e27d3eefd.png",
+            des: "Urine contains many dissolved minerals and salts. When the urine contains high le...        ",
+        },
+        {
+            id: 49,
+            name: "Leprosy",
+            image: "https://onemg.gumlet.io/a_ignore,w_64,h_64,q_auto,f_auto/chq8w9wixxakg8u3ttgk.png",
+            des: "Leprosy is a chronic infectious disease caused by a bacteria Mycobacterium leprae...        ",
+        },
+        {
+            id: 50,
+            name: "Liver Cirrhosis",
+            image: "https://onemg.gumlet.io/a_ignore,w_64,h_64,q_auto,f_auto/lppqzjhhutcgp6exn6nq.png",
+            des: "Cirrhosis is scarring (fibrosis) of the liver caused by long-term liver damage. T...        ",
+        },
+        {
+            id: 51,
+            name: "Low Immunity",
+            image: "https://onemg.gumlet.io/a_ignore,w_64,h_64,q_auto,f_auto/ekew5w82lz4neoaxlirz.png",
+            des: "Our body has a natural mechanism to fight against invading organisms and prevent ...        ",
+        },
+        {
+            id: 52,
+            name: "Lung Cancer",
+            image: "https://onemg.gumlet.io/a_ignore,w_64,h_64,q_auto,f_auto/ryeptv17bslrrjkpqfgm.png",
+            des: "Cancer is a medical condition in which the body starts to uncontrollably grow som...        ",
+        },
+
+        {
+            id: 53,
+            name: "Malaria",
+            image: "https://onemg.gumlet.io/a_ignore,w_64,h_64,q_auto,f_auto/tavhqgtlysldxcpqltga.png",
+            des: "Malaria is a parasitic infection that spreads when a mosquito infected with the P...            ",
+        },
+        {
+            id: 54,
+            name: "Measles",
+            image: "https://onemg.gumlet.io/a_ignore,w_64,h_64,q_auto,f_auto/uofftnmitla7qnvwntbk.png",
+            des: "Measles is a preventable, serious, highly contagious and acute febrile disease ca...          ",
+        },
+        {
+            id: 55,
+            name: "Migraine",
+            image: "https://onemg.gumlet.io/a_ignore,w_64,h_64,q_auto,f_auto/pkw825ouhsvzrhzgiegq.png",
+            des: "Migraine is a condition that causes intense, unprovoked pain, mostly on one or bo...            ",
+        },
+        {
+            id: 56,
+            name: "Monkeypox",
+            image: "https://onemg.gumlet.io/a_ignore,w_64,h_64,q_auto,f_auto/r647nbjqvoroi6qtaciq.png",
+            des: "Monkeypox is a viral zoonotic disease, which means that the infection spreads fro...            ",
+        },
+        {
+            id: 57,
+            name: "Mucormycosis",
+            image: "https://onemg.gumlet.io/a_ignore,w_64,h_64,q_auto,f_auto/nm8cch1oloc7hc4qmo8v.png",
+            des: "Mucormycosis is a rare but serious fungal infection triggered by a group of fungi...            ",
+        },
+        {
+            id: 58,
+            name: "Mumps",
+            image: "https://onemg.gumlet.io/a_ignore,w_64,h_64,q_auto,f_auto/dk7iouemikvt7bqmreht.png",
+            des: "Mumps is a contagious infection caused by a virus known as paramyxovirus. The vir...            ",
+        },
+        {
+            id: 59,
+            name: "Myocarditis",
+            image: "https://onemg.gumlet.io/a_ignore,w_64,h_64,q_auto,f_auto/mue940v9sfl97t87fqu3.png",
+            des: "Myocarditis is a medical condition in which the myocardium, a heart muscle, gets ...            ",
+        },
+
+
+
+    ]
+
+
+export const DataA =
+    [
+        {
+            id: 12,
+            name: "Acidity",
+            image: "https://onemg.gumlet.io/a_ignore,w_64,h_64,q_auto,f_auto/nc1b2nmmsjm0cpzzwuri.png",
+            des: "Acidity is one of the most common ailments that almost every one of us might have...",
+        },
+        {
+            id: 13,
+            name: "Acne",
+            image: "https://onemg.gumlet.io/a_ignore,w_64,h_64,q_auto,f_auto/r1lq3ouhxzhaeg2asehv.png",
+            des: "Acne is a common condition, which most of us have dealt with at some point in our...",
+        },
+        {
+            id: 14,
+            name: "Allergic Conditions",
+            image: "https://onemg.gumlet.io/a_ignore,w_64,h_64,q_auto,f_auto/w7jst3btu1n9jevpt9wm.png",
+            des: "Allergies are caused when the body’s immune system responds abnormally to an exte...",
+        },
+        {
+            id: 17,
+            name: "Amenorrhea",
+            image: "https://onemg.gumlet.io/a_ignore,w_64,h_64,q_auto,f_auto/agflfkldbzvjh7zohjdw.png",
+            des: "Amenorrhea is defined as the absence of menstruation during the reproductive year...",
+        },
+        {
+            id: 15,
+            name: "Anxiety",
+            image: "https://onemg.gumlet.io/a_ignore,w_64,h_64,q_auto,f_auto/xd0xxmvdwxnfxk8d3lig.png",
+            des: "Anxiety disorders are the most common types of mental health conditions. Accordin...",
+        },
+        {
+            id: 16,
+            name: "Asthma",
+            image: "https://onemg.gumlet.io/a_ignore,w_64,h_64,q_auto,f_auto/djlycbpy6uwh8tfiv9sf.png",
+            des: "Asthma is a long term condition in which air passages in the lungs become narrow ...",
+        },
+        {
+            id: 18,
+            name: "Autism",
+            image: "https://onemg.gumlet.io/a_ignore,w_64,h_64,q_auto,f_auto/marketing/2242beaf-32e3-4512-b1d4-7e0f2b1e8723.jpg",
+            des: "Autism, now known as autism spectrum disorder (ASD), refers to a broad range of c...",
+        },
+    ]
+
